7be23f34c79c839b4290c76bcb60e5e06753fb7a
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2024-03-04 17:51:32 +0700

    add print . process

--- engine.s
+++ engine.s
@@ -1,21 +1,15 @@
 section .data
-    NULL    equ 0
-    LF    equ 10
     num_to_print db "0"
     rem         db 0
-    result      db "0"
-    point       db '.',NULL
-    newLine     db LF,NULL
     denominator db 0
 section .text
 ; argument 1 (rdi) : denominator
 ; argument 2 (rsi) : remainder
-; argument 3 (rax) : remainder
 
-; rcx : loop counter
+; r10 : loop counter
 ; r11 : temp
-; r12 : total value of reminder
-; r13 : forward remainder
+; r12 : denominator
+; r13 : constant 10
 
 global  calculate_remainder
 extern print_string
@@ -24,7 +18,6 @@ calculate_remainder:
     push    rcx
     push    rdx     
     push    r10
-    push    r11
     push    r12
     push    r13
     push    r14
@@ -32,18 +25,7 @@ calculate_remainder:
     mov     [rem], rsi
     mov     [denominator], rdi
 
-    add     rdx, '0'
-    mov     byte [result], dl
-    mov     rdi, result
-    call    print_string
-
-    mov     rdi, point
-    call    print_string
-
-    ; mov     rdi, newLine
-    ; call    print_string
-
-    movzx     rax, byte [rem]
+    movzx   rax, byte [rem]
     mov     r10, 4  ; set loop counter to 4 round
     mov     r12, [denominator]
     mov     r13, 10
@@ -57,21 +39,18 @@ my_loop:
     mov     byte [num_to_print], al
     mov     rdi, num_to_print
     call    print_string
-    movsx   rax, byte [rem]
+    movzx   rax, byte [rem]
     dec     r10
     cmp     r10, 0
     jne     my_loop
     jmp     end_of_process
 
 end_of_process:
-    mov     rdi, newLine
-    call    print_string
     mov     rax, 0
     ; restore data back to register
     pop     r14
     pop     r13
     pop     r12
-    pop     r11
     pop     r10
     pop     rdx
     pop     rcx
--- main.s
+++ main.s
@@ -2,20 +2,33 @@ global _start
 section .data
     a           db  66
     b           db  7
-
+    result      dw  0
+    rem         db  0
+    dot         db ".",0
 extern  calculate_remainder
-extern  print_num
 extern  print_string
-
 
 section .text
 _start:
     mov     rdx, 0          ; clear reg edx for remainder
     movzx   rax, byte [a]   ; set divide variable into register rax, rbx then div
     movzx   rbx, byte [b]
-    div     rbx
+    div     rbx ; rax = a/b, rdx = a%b
+
+    ; convert result in rax to string then print
+    add     rax, '0'
+    mov     word [result], ax
+    mov     byte [rem], dl
+    mov     rdi, result
+    call    print_string
+
+    ; print "."
+    mov     rdi, dot
+    call    print_string
+
+    ; forward rem and denominator to calculate_remainder func
     movzx   rdi, byte [b]
-    mov     rsi, rdx
+    movzx   rsi, byte [rem]
     mov     rdx, rax
     call    calculate_remainder
 
